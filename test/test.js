e35b7bccff4a866a2e8697327245a0993d8f1ea6
The Architect <timmcgowan@syndromesoftware.com>
2012-05-25 14:02:05 -0500

    Ignores xml that is only a doctype. Closes #1
    Added vows testing

---
+++ test/test.js
@@ -0,0 +1,50 @@
+// string test.js
+// why does this not work?
+
+var vows = require('vows');
+var assert = require('assert');
+
+var parser = require('../lib/feed.js');
+
+vows.describe('bindparser').addBatch({
+
+  'rss tests':{
+    topic:function(){
+      parser.parseURL('http://rss.cnn.com/rss/cnn_topstories.rss', {}, this.callback);
+    },
+    'reponse is not null':function(err, docs){
+      assert.isNull(err);
+      assert.isNotNull(docs);
+    },
+    'response is properly formatted':function(err, docs){
+      assert.equal(docs.type, 'rss');
+      assert.isObject(docs.metadata);
+      assert.isArray(docs.items);
+    }
+  },
+  'atom tests':{
+    topic:function(){
+      parser.parseURL('http://www.blogger.com/feeds/10861780/posts/default', {}, this.callback);
+    },
+    'reponse is not null':function(err, docs){
+      assert.isNull(err);
+      assert.isNotNull(docs);
+    },
+    'response is properly formatted':function(err, docs){
+      assert.equal(docs.type, 'atom');
+      assert.isObject(docs.metadata);
+      assert.isArray(docs.items);
+    }
+  },
+  'oddities':{
+    'empty xml':{
+      topic:function(){
+        parser.parseString('<?xml version="1.0" ecoding="UTF-8"?>', {}, this.callback);
+      },
+      'returns an error && docs is null':function(err, docs){
+        assert.isNotNull(err);
+        assert.isNull(docs);
+      }
+    }
+  }
+}).export(module);
